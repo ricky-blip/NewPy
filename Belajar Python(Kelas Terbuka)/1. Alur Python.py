04f407d39b87b731189c39c8cfddeb266c69794f
ricky-blip <ricky.rinaldy77@gmail.com>
2021-11-02 10:28:15 +0700

    update belajar

---
+++ Belajar Python(Kelas Terbuka)/1. Alur Python.py
@@ -0,0 +1,14 @@
+print("Hello")
+print("World")
+print("Hello World")
+
+print("Halo Python")
+a = 10 
+# ini adalah cara comment
+"""
+ini adalah cara comment multiline
+"""
+print(a * 9)
+
+# kita bisa mengcompile python ke 
+# yang namanya bytecode
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+'''
+Semua Sintaksis Dasar bahasa Pemrograman terdiri dari:
+1. Sekuensial   : Langkah Berurutan
+2. Percabangan  : Langkah melompat jika kondisi terpenuhi
+3. Perulangan   : Mengulang Langkah yang sama berkali-kali sampai kondisi terpenuhi
+'''
+
+#Sekuensial example code
 print('Hola Senorita')
