9186616340039b76eeb5db4f22c8eaa086affb94
ricky-blip <ricky.rinaldy77@gmail.com>
2021-11-24 07:26:27 +0700

    update

---
+++ Belajar Python(Kelas Terbuka)/2-Variabel.py
@@ -0,0 +1,20 @@
+
+x = 2
+y = x + 4
+
+#pemanggilan pertama
+print("Nilai y adalah",y)
+
+#penamaan
+nilai_r = 15 #dengan menggunakan underscore
+juta10 = 10000000 #variabel tidak boleh dimulai dari angka
+nilaiX = 17.5 #nilai boleh decimal
+
+#pemanggilan kedua
+print("Nilai y adalah",y)
+y = 5 #menimpa variabel sebelumnya
+print("Nilai y adalah",y)
+
+#assigment indirect
+R = y 
+print("Nilai r adalah",R) #nilai R adalah variabel y yang terakhir
