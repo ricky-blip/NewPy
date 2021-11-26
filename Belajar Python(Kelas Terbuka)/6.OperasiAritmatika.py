ce1b769270099e60fad1826f314466b55d54b2f4
ricky-blip <ricky.rinaldy77@gmail.com>
2021-11-27 06:29:44 +0700

    update

--- Belajar Python(Kelas Terbuka)/6.OperasiAritmatika.py
+++ Belajar Python(Kelas Terbuka)/6.OperasiAritmatika.py
@@ -27,3 +27,38 @@ i = 5
 j = 5
 pangkat = i ** j
 print(i, "**", j, "=", pangkat)
+
+print("----Operasi Modulus atau sisa bagi ( % )----")
+k = 20 
+l = 3
+mod = k % l
+print(k, "%", l, "=", mod)
+
+print("----Operasi Floor Division kebalikan dari MOD ( // )----")
+m = 20 
+n = 3
+mod = m // n
+print(m, "//", n, "=", mod)
+
+print ("==========================================")
+print ("prioritas operasi, operational precendence")
+print ("==========================================")
+
+'''
+    1. ()
+    2. exponen atau pangkat **
+    3. perkalian *
+    4. pembagian /
+    5. modulus %
+    6. floor divison //
+    7. tambah +
+    8. kurang -
+'''
+a = 3
+b = 2
+c = 4
+
+hasil = a ** b * c + a / b - b % c // a
+
+print(a,'**',b,'*',c,'+',a,'/',b,'-',b,'%',c,'//',a,'=',hasil) 
+
