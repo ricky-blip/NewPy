35c3108ae61a64f46859c7e40718089a30416384
ricky-blip <ricky.rinaldy77@gmail.com>
2021-11-28 21:40:40 +0700

    update

---
+++ RoadmapPython/7.OperasiKomparasi.py
@@ -0,0 +1,86 @@
+#Comparison Operator
+
+# setiap hasil dari operasi komparasi adalah boolean (True False)
+
+# > , < , >= , <= , == , != , is , isnot
+
+print("==== lebih besar dari '>' =====")
+a = 1 
+b = 2
+
+hasil = a > 0
+print(a,'>',0, '=', hasil)
+
+hasil = b > a
+print(b,'>',a, '=', hasil)
+
+hasil = b > 2
+print(b,'>',2, '=', hasil)
+
+print("==== lebih kecil dari '<' =====")
+c = 3 
+d = 4
+
+hasil = c < 1
+print(c,'<',1, '=', hasil)
+
+hasil = d < 5
+print(d,'<',c, '=', hasil)
+
+hasil = d < 4
+print(d,'<',4, '=', hasil)
+
+print("==== lebih besar sama dengan '>=' =====")
+e = 5 
+f = 6
+
+hasil = e >= 2
+print(e,'>=',2, '=', hasil)
+
+hasil = f >= 9
+print(f,'>=',3, '=', hasil)
+
+hasil = f >= 6
+print(f,'>=',6, '=', hasil)
+
+print("==== lebih kecil sama dengan '<=' =====")
+g = 7 
+h = 8
+
+hasil = g <= 2
+print(g,'<=',2, '=', hasil)
+
+hasil = h <= 9
+print(h,'<=',9, '=', hasil)
+
+hasil = h <= 8
+print(h,'<=',8, '=', hasil)
+
+print("==== sama dengan '==' =====")
+i = 9 
+j = 10
+
+hasil = i == 2
+print(i,'==',2, '=', hasil)
+
+hasil = j == 9
+print(j,'==',9, '=', hasil)
+
+hasil = j == 10
+print(j,'==',10, '=', hasil)
+
+print("==== tidak sama dengan '!=' =====")
+k = 11 
+l = 12
+
+hasil = k != 2
+print(k,'!=',2, '=', hasil)
+
+hasil = l != 9
+print(l,'!=',9, '=', hasil)
+
+hasil = l != 12
+print(l,'!=',12, '=', hasil)
+
+
+
