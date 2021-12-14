0bb8227c4a265d772086e3c0272bc83e99b1bd50
ricky-blip <ricky.rinaldy77@gmail.com>
2021-12-14 08:18:35 +0700

    update

---
+++ RoadmapPython/39.InstallPackage.py
@@ -0,0 +1,14 @@
+# Make sure you install Python
+# install pip
+# install package (Numpy)
+
+print(15*"=" + "Use Numpy" + 15*"=")
+
+import numpy as np
+
+a = np.array([1,2,3])
+
+b = np.array([4,5,6])
+
+print(a)
+print(b)
